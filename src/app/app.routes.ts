b619ec33a59f6bc1551dc5624b680f034c95bdce
yadasai007 <yadasai007@gmail.com>
2025-04-08 12:15:30 -0400

    new files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { Component } from '@angular/core';
+import { SkillsComponent } from './skills/skills.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'experience',component:ExperienceComponent},
+    {path:'projects',component:ProjectsComponent},
+    {path:'skills',component:SkillsComponent}
+
+];
